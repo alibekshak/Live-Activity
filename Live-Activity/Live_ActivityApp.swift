37552c8c22f0499ca80a40b5b8c0ff7c0c0dab42
alibekshak <alibekshakiro1@gmail.com>
2026-06-01 07:55:53 +0500

    first commit

---
+++ Live-Activity/Live_ActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Live_ActivityApp.swift
+//  Live-Activity
+//
+//  Created by Alibek Shakirov on 01.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct Live_ActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
